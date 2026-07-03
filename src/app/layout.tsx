b51768a9debb8baaccc8d74f84523b730176d178
KriawqZero <marciliortizz@gmail.com>
2026-07-03 11:46:47 -0400

    feat: integrate Clerk with localization and custom theme configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ptBR } from "@clerk/localizations";
+import { dark } from "@clerk/ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ClerkProvider>
+        <ClerkProvider localization={ptBR} appearance={{ theme: [dark, 'simple'] }}>
           {children}
         </ClerkProvider>
       </body>
